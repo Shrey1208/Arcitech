da3cee6e9506b8e360fa53edfd01c3bd16961759
Shrey1208 <shreyashpathak755@gmail.com>
2023-12-14 10:31:02 +0530

    First commit

---
+++ task_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+
+    def __str__(self):
+        return self.title
